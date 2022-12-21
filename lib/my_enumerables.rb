77ff3a048589aa1227eec1d3af8465768c940ee6
Nina <cfratto87@gmail.com>
2022-12-21 11:52:58 -0600

    custom enumerable project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,32 @@
 module Enumerable
-  # Your code goes here
+  def my_each
+  end
+
+  def my_each_with_index
+  end
+
+  def my_all
+  end
+
+  def my_any
+  end
+  
+  def my_count
+  end
+
+  def my_inject
+  end
+
+  def my_map
+  end
+  
+  def my_none
+  end
+
+  def my_select
+  end
+
+  
 end
 
 # You will first have to define my_each
@@ -7,5 +34,67 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    return self unless block_given?
+    i = 0
+    while i < length
+      yield self[i]
+      i += 1
+    end
+  end
+
+  def my_each_with_index
+    return self unless block_given?
+    if self.is_a?(Array)
+      self.size.times {|index| yield self[index],index}
+      self
+    end
+  end
+
+  def my_all?
+    result = []
+    self.my_each {|x| result << x if yield(x)}
+    result == self
+  end
+
+  def my_any
+    result = []
+    self.my_each {|x| return yield(x)}
+  end
+  
+  def my_count
+    return self.length unless block_given?
+    result = []
+    self.my_each {|x| result << x if yield(x)}
+    return result.length
+  
+  end
+
+  #Still need to work on
+  def my_inject(i = nil)
+    result = self[i]
+    while i < length - 1
+        result = yield(result, self[i + 1])
+        i += 1
+    end
+    return result
+  end
+
+
+  def my_map
+    result = []
+    self.my_each {|x| result << yield(x)}
+    return result
+  end
+  
+  def my_none?
+    result = []
+    self.my_each {|x| return yield(x) ? false : true}
+  end
+
+  def my_select
+    result = []
+    self.my_each {|x| result << x if yield(x)}
+    return result
+  end
 end
